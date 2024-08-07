10ac78f98520cb565eae3b88c267cc1c195c28e9
liyuji <fackbing@hotmail.com>
2024-08-07 11:20:57 +0800

    添加文件

---
+++ src/easily.framework.webapi/Program.cs
@@ -0,0 +1,74 @@
+
+using Serilog;
+using Serilog.Events;
+
+namespace easily.framework.webapi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region ÅäÖÃSerilogÈÕÖ¾¼ÇÂ¼Æ÷
+            Log.Logger = new LoggerConfiguration()
+#if DEBUG
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+#else
+            .MinimumLevel.Warning()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+#endif
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Async(c => c.File("Logs/logs.log", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 1024 * 1024 * 5))
+#if DEBUG
+            .WriteTo.Async(c => c.Console())
+#endif
+            .CreateLogger();
+            #endregion
+
+            try
+            {
+                Log.Information("WebHost Starting...");
+                var builder = WebApplication.CreateBuilder(args);
+
+                // Add services to the container.
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                if (ex is HostAbortedException)
+                {
+                    throw;
+                }
+
+                Log.Fatal(ex, "Host terminated unexpectedly!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
